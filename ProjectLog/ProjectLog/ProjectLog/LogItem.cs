3621d67020df92ae9c082f7f9f4212398e0f2d1a
Septu Jamasoka <septu.jamasoka@gmail.com>
2014-01-20 10:31:59 +0700

    Solution Log

---
+++ ProjectLog/ProjectLog/ProjectLog/LogItem.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLog
+{
+    public class LogItem
+    {
+        public string controllerName { get; set; }
+        public string actionName { get; set; }
+        public string requestHeader { get; set; }
+        public string requestBody { get; set; }
+        public DateTime dateTime { get; set; }
+        public string ipAddress { get; set; }
+        public string rawUrl { get; set; }
+        public string fileSemiPath { get; set; }
+
+        public string username { get; set; }
+        public string user_id { get; set; }
+
+        public string message { get; set; }
+
+        public string generateMessage()
+        {
+            this.message = string.Format("{0} : {1}({4}) accessing /{2}/{3}", this.dateTime.ToString(), this.username, this.controllerName, this.actionName, this.user_id);
+            this.message += "\n" + "Raw Url : " + rawUrl;
+            this.message += "\n" + "IP Address : " + ipAddress;
+            this.message += "\n" + "Request Header : " + requestHeader;
+            this.message += "\n" + "Request Body : " + requestBody;
+            this.message += "\n" + "===========================================\n";
+
+            return this.message;
+        }
+
+        public bool saveLog()
+        {
+            generateMessage();
+            var date = this.dateTime.ToString("yyyyMMdd");
+            if (!File.Exists(this.fileSemiPath + "penting" + date + ".log"))
+            {
+                File.Create(this.fileSemiPath + "penting" + date + ".log").Close();
+            }
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(this.fileSemiPath + "penting" + date + ".log", true))
+            {
+                file.WriteLine(message);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ ProjectLog/ProjectLogging/ProjectLogging/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLogging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogItem log = new LogItem();
+            log.controllerName = "abcd";
+            log.actionName = "efgh";
+            log.requestHeader = "abcd";
+            log.requestBody = "efgh";
+            log.dateTime = DateTime.Now.AddDays(2);
+            log.ipAddress = "::1";
+            log.rawUrl = "/abcd/efgh/";
+            log.fileSemiPath = "d://";
+            log.saveLog();
+        }
+    }
+}
