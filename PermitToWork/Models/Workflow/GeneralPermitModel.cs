d47f0c511717c4a58c260e08f37440e7acb6df94
chandra <chandra@licht-soft.com>
2015-03-13 16:20:02 +0700

    fix general permit permit cancellation reject FO balik ke supervisor

--- PermitToWork/Models/Workflow/GeneralPermitModel.cs
+++ PermitToWork/Models/Workflow/GeneralPermitModel.cs
@@ -30,7 +30,7 @@ namespace PermitToWork.Models.Workflow
             cancellationSignals = signals.Where(m => CancellationNodeNameList.Contains(m.node_name)).ToList();
             if (cancellationSignals.Count() > 0)
             {
-                cancellationNodes = helper.ProcessSerialNodes(cancellationSignals, CancellationNodeNameList);
+                cancellationNodes = ProcessGPCancellationNodes(cancellationSignals, CancellationNodeNameList);
                 NodeList = NodeList.Concat(cancellationNodes).ToList();
             }
         }
@@ -39,6 +39,7 @@ namespace PermitToWork.Models.Workflow
         /// memproses list of signals
         /// kasus
         ///     assessor reject -> choosing assessor finish, supervisor screening ongoing
+        ///     facility owner reject -> assessor approval empty, supervisor screening ongoing
         /// </summary>
         /// <param name="signals">signals terkait workflow general permit saja, TIDAK TERMASUK signal cancellation</param>
         /// <param name="nodeNames"></param>
@@ -47,7 +48,7 @@ namespace PermitToWork.Models.Workflow
         {
             //kamus lokal
             List<string> approvedNodeNames = new List<string>(); //stack of node names
-            List<string> prevNodeNames;
+            List<string> nodeNamesToBeDeleted;
             string lastApproved;
             int nodeNameIndex;
             List<WorkflowNodePresentationStub> nodeList;
@@ -69,29 +70,29 @@ namespace PermitToWork.Models.Workflow
                 }
                 else if (node.status == (int)WorkflowNodeServiceModel.NodeStatus.REJECTED) //reject signal
                 {
-                    prevNodeNames = new List<string>();
+                    nodeNamesToBeDeleted = new List<string>();
                     lastApproved = approvedNodeNames.LastOrDefault();
                     if (lastApproved != null)
                     {
                         //kasus khusus reject assessor
                         if (node.node_name == WorkflowNodeServiceModel.GeneralPermitNodeName.ASSESSOR_APPROVE.ToString())
                         {
-                            prevNodeNames.Add(WorkflowNodeServiceModel.GeneralPermitNodeName.SUPERVISOR_APPROVE.ToString());
+                            nodeNamesToBeDeleted.Add(WorkflowNodeServiceModel.GeneralPermitNodeName.SUPERVISOR_APPROVE.ToString());
                         }
                         else if (node.node_name == WorkflowNodeServiceModel.GeneralPermitNodeName.FACILITY_OWNER_APPROVE.ToString())
                         {
-                            prevNodeNames.Add(WorkflowNodeServiceModel.GeneralPermitNodeName.ASSESSOR_APPROVE.ToString());
-                            prevNodeNames.Add(WorkflowNodeServiceModel.GeneralPermitNodeName.SUPERVISOR_APPROVE.ToString());
+                            nodeNamesToBeDeleted.Add(WorkflowNodeServiceModel.GeneralPermitNodeName.ASSESSOR_APPROVE.ToString());
+                            nodeNamesToBeDeleted.Add(WorkflowNodeServiceModel.GeneralPermitNodeName.SUPERVISOR_APPROVE.ToString());
                         }
                         else
                         {
                             nodeNameIndex = nodeNames.FindIndex(a => a == node.node_name);
-                            prevNodeNames.Add(nodeNames[nodeNameIndex - 1]);
+                            nodeNamesToBeDeleted.Add(nodeNames[nodeNameIndex - 1]);
                         }
 
-                        foreach (string prevNodeName in prevNodeNames)
+                        foreach (string nodeName in nodeNamesToBeDeleted)
                         {
-                            if (approvedNodeNames.Remove(prevNodeName) == false)
+                            if (approvedNodeNames.Remove(nodeName) == false)
                             {
                                 throw new Exception("Kesalahan dalam fungsi pemrosesan signal.");
                             }
@@ -124,6 +125,85 @@ namespace PermitToWork.Models.Workflow
                     {
                         break;
                     }
+                }
+            }
+
+            return nodeList;
+        }
+
+        /// <summary>
+        /// memproses list of cancellation signals
+        /// kasus
+        ///     assessor reject -> choosing assessor finish, supervisor screening ongoing
+        ///     facility owner reject -> assessor approval empty, supervisor screening ongoing
+        /// </summary>
+        /// <param name="signals">signals terkait workflow cancellation general permit saja</param>
+        /// <param name="nodeNames"></param>
+        /// <returns></returns>
+        private List<WorkflowNodePresentationStub> ProcessGPCancellationNodes(List<workflow_node> signals, List<string> nodeNames)
+        {
+            //kamus lokal
+            List<string> approvedNodeNames = new List<string>(); //stack of node names
+            List<string> nodeNamesToBeDeleted;
+            string lastApproved;
+            int nodeNameIndex;
+            List<WorkflowNodePresentationStub> nodeList;
+            bool existOngoing = false;
+
+            //algoritma
+            //inisiasi NodeList
+            nodeList = new List<WorkflowNodePresentationStub>();
+            foreach (string name in nodeNames)
+                nodeList.Add(new WorkflowNodePresentationStub { NodeName = name, Status = WorkflowNodePresentationStub.NodeStatus.EMPTY });
+
+            //mengisi nodes status ke stack
+            signals = signals.OrderBy(m => m.id).ToList(); //diurutkan dari paling lama ke paling baru
+            foreach (workflow_node node in signals)
+            {
+                if (node.status == (int)WorkflowNodeServiceModel.NodeStatus.APPROVED) //approve signal
+                {
+                    approvedNodeNames.Add(node.node_name);
+                }
+                else if (node.status == (int)WorkflowNodeServiceModel.NodeStatus.REJECTED) //reject signal
+                {
+                    nodeNamesToBeDeleted = new List<string>();
+                    lastApproved = approvedNodeNames.LastOrDefault();
+                    if (lastApproved != null)
+                    {
+                        //kasus khusus reject assessor
+                        if (node.node_name == WorkflowNodeServiceModel.GeneralPermitNodeName.CANCELLATION_FACILITY_OWNER.ToString())
+                        {
+                            nodeNamesToBeDeleted.Add(WorkflowNodeServiceModel.GeneralPermitNodeName.CANCELLATION_ASSESSOR.ToString());
+                            nodeNamesToBeDeleted.Add(WorkflowNodeServiceModel.GeneralPermitNodeName.CANCELLATION_SUPERVISOR.ToString());
+                        }
+                        else
+                        {
+                            nodeNameIndex = nodeNames.FindIndex(a => a == node.node_name);
+                            nodeNamesToBeDeleted.Add(nodeNames[nodeNameIndex - 1]);
+                        }
+
+                        foreach (string nodeName in nodeNamesToBeDeleted)
+                        {
+                            if (approvedNodeNames.Remove(nodeName) == false)
+                            {
+                                throw new Exception("Kesalahan dalam fungsi pemrosesan signal.");
+                            }
+                        }
+                    }
+                }
+            }
+
+            //set status setiap item di NodeList
+            foreach (WorkflowNodePresentationStub node in nodeList)
+            {
+                if (approvedNodeNames.Contains(node.NodeName))
+                {
+                    node.Status = WorkflowNodePresentationStub.NodeStatus.FINISH;
+                }
+                else
+                {
+                    node.Status = WorkflowNodePresentationStub.NodeStatus.ONGOING;
+                    break;
                 }
             }
 
